e4e50f68c32a692b033898cc97b5709075db98e0
kalpitzz <kalpitjangid247@gmail.com>
2021-05-02 14:30:27 +0530

    add cart ,auth page

--- server.js
+++ server.js
@@ -10,14 +10,19 @@ const PORT=process.env.PORT || 3000;
 //Assets
 app.use(express.static('public'));
 
-
-app.get('/',(req,resp)=>resp.render('home'));
-
-
 //set Template engine 
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+
+
+//App Routes
+app.get('/',(req,resp)=>resp.render('home'));
+app.get('/cart',(req,resp)=>resp.render('customers/cart'));
+app.get('/login',(req,resp)=>resp.render('auth/login'));
+app.get('/register',(req,resp)=>resp.render('auth/register'));
+
+
 
 
 app.listen(PORT, ()=> console.log(`Listening on kalpitzz port ${PORT}`));
